d074e872f0ae6f219594a1858835f256b859b14e
Allan Targino <allan_targino@hotmail.com>
2018-10-28 20:16:36 +0000

    detecting dotnet lang

--- docker/runtime/dotnetcore-2.0/compile-function.sh
+++ docker/runtime/dotnetcore-2.0/compile-function.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# kubeless function directory
 PROJECT_MOUNT=$1
-PACKAGES_DIR=$PROJECT_MOUNT/packages
-USER_CSPROJ=$PROJECT_MOUNT/project.csproj
-DEFAULT_CSPROJ=/app/project.csproj 
 
-if [ -s $USER_CSPROJ ]; then
-	echo .csproj present;
+# detect language
+if [ -s $PROJECT_MOUNT/*cs* ]; then
+	LANGUAGE=cs;
+elif [ -s $PROJECT_MOUNT/*fs* ]; then
+	LANGUAGE=fs;
 else
-	cp $DEFAULT_CSPROJ $USER_CSPROJ
+	LANGUAGE=vb;
 fi
 
+# set project files variables
+PACKAGES_DIR=$PROJECT_MOUNT/packages
+USER_PROJ=$PROJECT_MOUNT/project.${LANGUAGE}proj
+DEFAULT_CSPROJ=/app/project.csproj 
+
+# copy a default project file if not present
+if [ -s $USER_PROJ ]; then
+	echo Project file is here;
+else
+	cp $DEFAULT_CSPROJ $USER_PROJ
+fi
+
+# compile
 dotnet restore $PROJECT_MOUNT --packages $PACKAGES_DIR
 dotnet publish $PROJECT_MOUNT -o publish -c Release --no-restore
